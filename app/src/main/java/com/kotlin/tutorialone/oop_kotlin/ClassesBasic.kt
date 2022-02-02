83e5832aefd49e6d0306b86d5835a6c6a2c35e93
kalin <kalindusupun@gmail.com>
2022-02-02 19:02:25 +0530

    Kotlin-tutorials

--- app/src/main/java/com/kotlin/tutorialone/oop_kotlin/ClassesBasic.kt
+++ app/src/main/java/com/kotlin/tutorialone/oop_kotlin/ClassesBasic.kt
@@ -6,6 +6,22 @@ fun main(){
     var person2 = Person2("Kalindu","Supun");
     var person3 = Person3(); // Default values will print
     var person4 = Person3(lName = "Hettirachchi"); // fName Default values will print and lName passing value will print
+
+    var person5 = Person5(fName = "Kalidnu");
+    person5.getHobby()
+
+    person5.fName="Sudpun" //changing values overwritten
+    person5.hobby="Playing football" //changing values for public variables overwritten
+    person5.getHobby()
+
+
+    var person6 = Person5( "Kalidnu", "Supun",30);
+    person6.getHobbyAndAge()
+    person6.age=34 // age overwritten
+    person6.getHobbyAndAge()
+
+
+
 }
 
 class Person(){
@@ -16,7 +32,7 @@ class Person(){
     }
 }
 
-class Person2(var fName:String, var lName:String  ){ // passing value to constructor
+class Person2(fName:String, lName:String  ){ // passing value to constructor
 
     init{
         println("Second method $fName and $lName")
@@ -24,10 +40,38 @@ class Person2(var fName:String, var lName:String  ){ // passing value to constru
 
 }
 
-class Person3(var fName:String="DefaultFName", var lName:String="DefaultLName"  ){ // passing value to constructor
+class Person3(fName:String="DefaultFName", lName:String="DefaultLName"  ){ // passing value to constructor
 
     init{
         println("Second method $fName and $lName")
     }
+
+}
+
+class Person5(fName:String="DefaultFName", lName:String="DefaultLName"  ){ // passing value to constructor
+
+    var fName:String?=null
+    var lName:String?=null
+    var age:Int?=null
+    var hobby ="No hobby"
+
+    init{
+        this.fName=fName;
+        this.lName=lName;
+    }
+
+    constructor(fName:String, lName:String, age:Int):this(fName,lName){ // secondary constructor. fname lname get from above
+        this.age=age;
+    }
+
+    public fun getHobby(){
+        println("$fName hobby is $hobby")
+    }
+
+    public fun getHobbyAndAge(){
+        println("$fName $lName hobby is $hobby my age is $age")
+    }
+
+
 
 }
