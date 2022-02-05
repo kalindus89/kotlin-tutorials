7349cc964481b3ba6c901c53d6c5a2032c98f99e
kalin <kalindusupun@gmail.com>
2022-02-05 19:53:02 +0530

    Kotlin-tutorials

---
+++ app/src/main/java/com/kotlin/tutorialone/navigation_components/NavigationComponentActivity.kt
@@ -0,0 +1,12 @@
+package com.kotlin.tutorialone.navigation_components
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.kotlin.tutorialone.R
+
+class NavigationComponentActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_navigation_componet)
+    }
+}
