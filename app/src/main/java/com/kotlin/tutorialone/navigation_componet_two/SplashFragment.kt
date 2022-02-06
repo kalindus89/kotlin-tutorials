069800f224a34896d245aaa382ff48e1435c8177
kalin <kalindusupun@gmail.com>
2022-02-06 13:36:11 +0530

    Kotlin-tutorials

--- app/src/main/java/com/kotlin/tutorialone/navigation_componet_two/SplashFragment.kt
+++ app/src/main/java/com/kotlin/tutorialone/navigation_componet_two/SplashFragment.kt
@@ -1,10 +1,12 @@
 package com.kotlin.tutorialone.navigation_componet_two
 
 import android.os.Bundle
+import android.os.Handler
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.Navigation
 import com.kotlin.tutorialone.R
 
 
@@ -16,7 +18,14 @@ class SplashFragment : Fragment() {
         savedInstanceState: Bundle?,
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_splash, container, false)
+        val view= inflater.inflate(R.layout.fragment_splash, container, false)
+
+
+        Handler().postDelayed({
+            Navigation.findNavController(view).navigate(R.id.viewpagerFragment)
+        },3000)
+
+        return view
     }
 
 
--- app/src/main/java/com/kotlin/tutorialone/navigation_componet_two/on_boarding/ViewpagerFragment.kt
+++ app/src/main/java/com/kotlin/tutorialone/navigation_componet_two/on_boarding/ViewpagerFragment.kt
@@ -5,7 +5,11 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.viewpager2.widget.ViewPager2
 import com.kotlin.tutorialone.R
+import com.kotlin.tutorialone.navigation_componet_two.on_boarding.fragments_screens.FirstFragmentScreen
+import com.kotlin.tutorialone.navigation_componet_two.on_boarding.fragments_screens.SecondFragmentScreen
+import com.kotlin.tutorialone.navigation_componet_two.on_boarding.fragments_screens.ThirdFragmentScreen
 
 
 class ViewpagerFragment : Fragment() {
@@ -17,6 +21,15 @@ class ViewpagerFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         val view= inflater.inflate(R.layout.fragment_viewpager, container, false)
+
+        val fragmentLists = arrayListOf<Fragment>(FirstFragmentScreen(),SecondFragmentScreen(),ThirdFragmentScreen())
+
+        val viewPagerAdapter =ViewPagerAdapter(fragmentLists,requireActivity().supportFragmentManager,lifecycle)
+
+        val view_pager=view.findViewById<ViewPager2>(R.id.view_pager)
+
+        view_pager.adapter=viewPagerAdapter
+
 
         return view
     }
