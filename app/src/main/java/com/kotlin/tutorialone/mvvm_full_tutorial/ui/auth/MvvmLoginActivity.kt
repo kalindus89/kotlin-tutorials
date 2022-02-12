458d93e569a76b3704ddb432b6c662211e0fee6c
kalin <kalindusupun@gmail.com>
2022-02-12 11:44:27 +0530

    Kotlin-tutorials

---
+++ app/src/main/java/com/kotlin/tutorialone/mvvm_full_tutorial/ui/auth/MvvmLoginActivity.kt
@@ -0,0 +1,14 @@
+package com.kotlin.tutorialone.mvvm_full_tutorial.ui.auth
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.kotlin.tutorialone.R
+
+class MvvmLoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.mvvm_login_activity)
+
+
+    }
+}
---
+++ app/src/main/java/com/kotlin/tutorialone/mvvm_full_tutorial/ui/auth/MvvmSignUpActivity.kt
@@ -0,0 +1,12 @@
+package com.kotlin.tutorialone.mvvm_full_tutorial.ui.auth
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.kotlin.tutorialone.R
+
+class MvvmSignUpActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.mvvm_sign_up_activity)
+    }
+}
